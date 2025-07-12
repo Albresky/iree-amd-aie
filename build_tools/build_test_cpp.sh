2a409c6ddc2509b99015e8805bdb1546e3edc308
Albresky <albre02@outlook.com>
2025-07-12 13:37:04 +0800

    Enable tracing for HAL and VM; logging build/install progresses

--- build_tools/build_test_cpp.sh
+++ build_tools/build_test_cpp.sh
@@ -19,6 +19,8 @@ cache_dir="${cache_dir:-}"
 llvm_install_dir="${llvm_install_dir:-}"
 assertions="$1"
 enable_ctest="$2"
+build_type=RELEASE # Set to `Debug` to enable symbol debugging. Default for official is `Release`.
+
 
 # Setup cache dir.
 if [ -z "${cache_dir}" ]; then
@@ -65,12 +67,32 @@ echo '{
 cd $iree_dir
 CMAKE_ARGS=(
   -GNinja
-  -DCMAKE_BUILD_TYPE=Release
+  -DCMAKE_BUILD_TYPE=$build_type
   -DCMAKE_INSTALL_PREFIX="$install_dir"
   -DCMAKE_INSTALL_LIBDIR=lib
-  -DIREE_ERROR_ON_MISSING_SUBMODULES=OFF
+  -DCMAKE_OBJECT_PATH_MAX=4096
+
   -DENABLE_XRT_LITE_CTS_TESTS=ON
+
+  -DIREE_ERROR_ON_MISSING_SUBMODULES=OFF
   -DIREE_ENABLE_ASSERTIONS=$assertions
+  
+  # HAL API tracing, defaults to OFF
+  # This will show all the HAL APIs that have `IREE_TRACE_ZONE_BEGIN ... IREE_TRACE_ZONE_END`
+  # that are hit during a run/execution (of, e.g., iree-run-module).
+  -DIREE_ENABLE_RUNTIME_TRACING=ON
+  -DIREE_TRACING_PROVIDER=console
+  // optional but recommended
+  -DIREE_TRACING_CONSOLE_FLUSH=1
+
+  # Enable VM execution tracing, defaults to OFF
+  # This will show all of the VM dispatches that actually occur during a run/execution. 
+  # Note, this is roughly equivalent to passing `--compile-to=vm` to `iree-compile`.
+  -DIREE_VM_EXECUTION_TRACING_ENABLE=1
+  -DIREE_VM_EXECUTION_TRACING_FORCE_ENABLE=1
+  // optional
+  -DIREE_VM_EXECUTION_TRACING_SRC_LOC_ENABLE=1
+  
   -DIREE_BUILD_SAMPLES=OFF
   -DIREE_BUILD_PYTHON_BINDINGS=ON
   -DIREE_BUILD_BINDINGS_TFLITE=OFF
@@ -82,7 +104,6 @@ CMAKE_ARGS=(
   -DIREE_INPUT_TOSA=OFF
   -DIREE_INPUT_STABLEHLO=OFF
   -DIREE_INPUT_TORCH=ON
-  -DCMAKE_OBJECT_PATH_MAX=4096
   -DIREE_CMAKE_PLUGIN_PATHS="$repo_root"
 )
 
@@ -131,34 +152,35 @@ elif [[ "$OSTYPE" == "darwin"* ]]; then
   )
 fi
 
-cmake "${CMAKE_ARGS[@]}"
+cmake "${CMAKE_ARGS[@]}" 2>&1 | tee "$build_dir/cmake.log"
 
 echo "Building all"
 echo "------------"
-cmake --build "$build_dir" -- -k 0
+cmake --build "$build_dir" -- -k 0 2>&1 | tee "$build_dir/build.log"
 
 echo "Installing"
 echo "----------"
 echo "Install to: $install_dir"
-cmake --build "$build_dir" --target install
+cmake --build "$build_dir" --target install 2>&1 | tee "$build_dir/install.log"
 # TODO(max): there's no way to install the python runtime bindings
 # ninja iree-install-dist install-IREECompilerPythonModules install-IREEDialectsPythonModules
-cmake --build "$build_dir" --target iree-install-dist
+cmake --build "$build_dir" --target iree-install-dist 2>&1 | tee "$build_dir/iree-install-dist.log"
 
 
 if [[ "$enable_ctest" == "ON" ]]; then
   echo "CTest"
   echo "-----"
   if [[ "$OSTYPE" == "linux"* ]] && [[ "$assertions" == "ON" ]]; then
-    ctest --test-dir "$build_dir" -R amd-aie -E "driver" --output-on-failure -j
+    ctest --test-dir "$build_dir" -R amd-aie -E "driver" --output-on-failure -j 2>&1 | tee "$build_dir/ctest_linux.log"
   elif [[ "$OSTYPE" == "darwin"* ]]; then
-    ctest --test-dir "$build_dir" -R amd-aie -E "matmul_pack_peel_air_e2e|matmul_elementwise_pack_peel_air_e2e" --output-on-failure -j --repeat until-pass:5
+    ctest --test-dir "$build_dir" -R amd-aie -E "matmul_pack_peel_air_e2e|matmul_elementwise_pack_peel_air_e2e" --output-on-failure -j --repeat until-pass:5 2>&1 | tee "$build_dir/ctest_darwin.log"
   fi
 else
   echo "Skipping CTest"
 fi
 
 if [ -d "$llvm_install_dir" ]; then
+  echo "Copying LLVM tools($llvm_install_dir) to install directory($install_dir)"
   cp "$llvm_install_dir/bin/lld" "$install_dir/bin"
   cp "$llvm_install_dir/bin/FileCheck" "$install_dir/bin"
   cp "$llvm_install_dir/bin/not" "$install_dir/bin"
@@ -166,6 +188,7 @@ fi
 
 cp "$build_dir/tools/testing/e2e/iree-e2e-matmul-test" "$install_dir/bin"
 if [[ "$OSTYPE" == "linux"* ]]; then
+  echo "Copying AMD-AIE CTS tests to install directory($install_dir)"
   mkdir -p "$install_dir/device_tests"
   cp "$build_dir"/runtime/plugins/AMD-AIE/iree-amd-aie/driver/xrt-lite/cts/*test "$install_dir/device_tests"
 fi
