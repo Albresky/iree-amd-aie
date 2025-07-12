a4b35b5160b78e423dcd7f0055c031803f1f05b9
Albresky <albre02@outlook.com>
2025-07-12 20:37:47 +0800

    Setup env variables to enable device codegen (HAL&xclbin)

--- setup.sh
+++ setup.sh
@@ -7,3 +7,7 @@ export PATH="$(pwd)/iree-install/bin:$PATH"
 
 echo "Setting up IREE Python Bindings environment"
 source iree-build/.env && export PYTHONPATH
+
+echo "Setting up Vitis"
+export VITIS="/usr/xilinx/Vitis/2024.2"
+export XILINXD_LICENSE_FILE="/usr/xilinx/License/AIE.lic"
